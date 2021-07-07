eefc443a34d81cf195120679ae2db0b8657b3267
Aaron Dereszynski <aarondski123@gmail.com>
2021-07-07 17:25:22 -0600

    added name to container

--- js/index.js
+++ js/index.js
@@ -6,15 +6,20 @@ getFetch()
 
 
 function getFetch(){
-    fetch('http://localhost:3000/monsters/?_limit50')
+    fetch('http://localhost:3000/monsters/?_limit=50')
     .then(response => response.json())
     .then(json => pageLoad(json))
 }
 
 
-function pageLoad(nameInfo){
-    console.log(nameInfo)
-    document.querySelector("#monster-container").textContent = nameInfo.Object.keys()
+function pageLoad(monstersArr){
+    console.log(monstersArr)
+    let monstcontain = document.querySelector('#monster-container')
+    monstersArr.forEach(monsterObject => {
+        let monstName = document.createElement("h2")
+        monstName.textContent = monsterObject.name
+        monstcontain.append(monstName)
+    })
 
 
 
