22795e1bbbc2e276c6b2a39fba5bd12be41583c0
Aaron Dereszynski <aarondski123@gmail.com>
2021-07-07 16:22:32 -0600

    adding funtions

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
-fetch('http://localhost:3000/monsters')
-.then(response => response.json())
-.then(json => console.log(json))
+const monsterUrl = 'http://localhost:3000/monsters';
 
+funtion getFetch(){
+    fetch('http://localhost:3000/monsters')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    pageload()
+}
+
+
+funtion pageLoad(){
+
+
+}
